64720b4ab0b02a6aae0bc3ab8df0ef32291d3a24
Rob Harting <robharting@hotmail.com>
2024-08-30 15:49:43 +0200

    implemented first assignment implement UIPickerView functionality

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -14,6 +14,8 @@ struct CoinManager {
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for selectedCurrency: String){
+        print("CoinPrice of \(selectedCurrency)")
+    }
 }
